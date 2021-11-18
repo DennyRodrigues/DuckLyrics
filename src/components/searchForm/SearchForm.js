784ccddeeed0d52e3ae77c4ae8b616bd24c2f48e
DennyRodrigues <denny.rodrigues.carmo@gmail.com>
2021-11-18 16:26:45 -0300

    commit on mobile

--- src/components/searchForm/SearchForm.js
+++ src/components/searchForm/SearchForm.js
@@ -1,26 +1,36 @@
-import {useContext, useState} from 'react';
-import ContextSearch from '../store/ContextSearch';
-import styles from './SearchForm.module.css'
+import { useContext, useState } from "react";
+import ContextSearch from "../store/ContextSearch";
+import styles from "./SearchForm.module.css";
 
-function SearchForm(props) {
-  const [songTitle, setSongTitle] = useState('')
-  const contextQuery =  useContext(ContextSearch);
+const SearchForm = (props) => {
+  // Hold the input value, it will update everytime the user enter a new keystroke
+  const [songTitle, setSongTitle] = useState("");
+
+  const contextSearch = useContext(ContextSearch);
+
+  // When the user submit the form
   const submitHandler = (event) => {
-     event.preventDefault();
-     let songTitleSubmited = songTitle;
-     contextQuery.search(songTitleSubmited);
-     setSongTitle("");
-  }
+    event.preventDefault();
+    let songTitleSubmited = songTitle;
+    contextSearch.search(songTitleSubmited);
+    setSongTitle("");
+  };
+  // Update everytimte the user changes the form
   const updateSongTitle = (event) => {
-    setSongTitle(event.target.value)
-  }
+    setSongTitle(event.target.value);
+  };
 
   return (
     <form className={styles.SearchForm} onSubmit={submitHandler}>
       <label> Search for lyrics </label>
-      <input type="text" onChange={updateSongTitle} value={songTitle} />
+      <input
+        type="text"
+        onChange={updateSongTitle}
+        value={songTitle}
+        autoFocus={true}
+      />
       <button type="submit">Search</button>
     </form>
   );
-}
+};
 export default SearchForm;
